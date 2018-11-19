ecf9050469b6e734644e81d8239cb15473a8cc81
Nozominakamoto <nozomi.nakamoto.likeit@gmail.com>
2018-11-19 13:52:24 +0900

    再提出

--- Q1.sql
+++ Q1.sql
@@ -1,4 +1,5 @@
-select
-*
-from
-user;
+CREATE TABLE `item_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(256) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;INCREMENT=9 DEFAULT CHARSET=utf8;
